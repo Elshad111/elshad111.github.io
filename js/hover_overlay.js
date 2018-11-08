4be3c35df5d9deb8e69a89b419ceee57d5dd0f2d
Elshad <safarov.elshad007@gmail.com>
2018-11-08 14:42:46 +0500

    Added file hover_overlay.js

---
+++ js/hover_overlay.js
@@ -0,0 +1,22 @@
+$(function(){
+	var item = $('.team_item');
+	for(var i = 0; i < item.length; i++){
+		item.eq(i).on('mouseenter', function(event){
+			$(this).find('.team_item_overlay').stop(true,true);
+			$(this).find('.team_item_overlay').css({
+				'display' : 'block'
+			}).animate({
+				'opacity' : 1
+			}, 300, function(){
+				$(this).addClass('item_overlay_active').removeAttr('style');
+			}); 
+		}).on('mouseleave', function(event){
+			$(this).find('.team_item_overlay').stop(true,true);
+			$(this).find('.team_item_overlay').animate({
+				'opacity' : 0
+			},300, function(){
+				$(this).removeClass('item_overlay_active').removeAttr('style');
+			})
+		});
+	}
+});
